9739019bc81b2278e68f019f9099038667fe8e48
Chau Pham <minhchaupham90@gmail.com>
2019-09-21 19:04:45 +0200

    rm comment

--- JavaScript3/homework/index.js
+++ JavaScript3/homework/index.js
@@ -69,21 +69,6 @@
         }
     };
 
-    // const doSort = data => {
-    //     let da;
-    //     for (let i = 0; i < data.length; i++) {
-    //         da = Object.keys(data[i].name);
-    //         var sortable = [];
-    //         for (var name in da) {
-    //             sortable.push([name, data.name]);
-    //         }
-
-    //         sortable.sort(function(a, b) {
-    //             return a[1] - b[1];
-    //         });
-    //     }
-    // };
-
     // column left
 
     const makeInfoRepos = (data, element) => {
@@ -138,22 +123,8 @@
     };
 
     const addCSS = () => {
-        // document.querySelector('#ul').style.cssText =
-        //     '-webkit-column-count: 4; -moz-column-count: 4; column-count: 2; list-style-type: none;';
-        // document.querySelector('#ul .li').style.cssText =
-        //     '  text-align: center; display: inline-block; width: 100%;padding-bottom: 20px;';
-        // document.querySelector('.img').style.cssText =
-        //     ' width: 128px; -webkit-border-radius: 50% ; -moz-border-radius: 50% ; border-radius: 50% ;';
-        // document.querySelector('#container').style.cssText =
-        //     'display : flex; justify-content : space-between';
-
-        // document.querySelector('#ul').style.cssText =
-        //     '-webkit-column-count: 4; -moz-column-count: 4; column-count: 2; list-style-type: none;';
         document.querySelector('#container .leftColumn').style.cssText = 'flex : 1';
         document.querySelector('.rightColumn').style.cssText = ' flex : 2';
-
-        // document.querySelector('#a').style.cssText =
-        //     'display: block; font-size: 0.9em; text-decoration: none; font-family: "Open Sans", helvetica, arial, sans-serif;';
     };
 
     const REPOS_URL = 'https://api.github.com/orgs/foocoding/repos?per_page=100';
