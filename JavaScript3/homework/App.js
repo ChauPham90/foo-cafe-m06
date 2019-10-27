d2527d57dfeaf6daf12ba6e8006d159657c6bc10
Chau Pham <minhchaupham90@gmail.com>
2019-10-27 09:06:08 +0100

    uncompleted version split to multi-files

--- JavaScript3/homework/App.js
+++ JavaScript3/homework/App.js
@@ -7,6 +7,7 @@ class App {
 
     async initialize(url) {
         const root = document.getElementById('root');
+        Repository.CreateStructure(root)
         const header = Util.createAndAppend('header', root, { class: 'header' }); // TODO: replace with your own code
         const h1 = Util.createAndAppend('h1', header, { text: ' FOO M06' });
 
@@ -42,7 +43,7 @@ class App {
                     event.stopPropagation();
                     let val = document.querySelector('#' + e.repository.name);
                     if (val == null) {
-                        val = createListE(e.repository, ulE);
+                        val = Repository.createListE(e.repository, ulE);
                         onReposLoaded(index)
 
                     }
@@ -84,46 +85,6 @@ class App {
     renderError(error) {
         console.log(error);
     }
-}
-
-const createListE = (data, element) => {
-    const divContentE = document.querySelector('.content')
-    const sectionE = Util.createAndAppend('section', divContentE, {
-        id: data.name
-    });
-    const h2E = Util.createAndAppend('h2', sectionE, {
-        text: 'MODULE ',
-        class: 'h2'
-    });
-    Util.createAndAppend('span', h2E, {
-        text: data.name,
-        id: 'span'
-    });
-    const divE = Util.createAndAppend('div', sectionE, {
-        id: 'repoInfo',
-    });
-    const moduleDetail = Util.createAndAppend('div', divE, {
-        class: 'moduleDetail'
-
-    });
-    Util.createAndAppend('a', moduleDetail, {
-        class: data.name,
-        href: data.html_url,
-        text: 'Github link'
-    });
-    Util.createAndAppend('p', moduleDetail, {
-        text: `Description: ${data.description}`
-    });
-    Util.createAndAppend('p', moduleDetail, {
-        text: `Forks : ${data.forks_count}`
-    });
-    Util.createAndAppend('p', moduleDetail, {
-        text: `Updated : ${data.updated_at}`
-    });
-    Util.createAndAppend('div', divE, {
-        class: 'contri',
-    });
-
 }
 
 
--- JavaScript3/homework/Repository.js
+++ JavaScript3/homework/Repository.js
@@ -12,14 +12,48 @@ class Repository {
      * Render the repository info to the DOM.
      * @param {HTMLElement} container The container element in which to render the repository.
      */
-    render(container) {
-        // TODO: replace the next line with your code.
-        Util.createAndAppend('pre', container, JSON.stringify(this.repository, null, 2));
+
+    static createListE(data, element) {
+        const divContentE = document.querySelector('.content')
+        const sectionE = Util.createAndAppend('section', divContentE, {
+            id: data.name
+        });
+        const h2E = Util.createAndAppend('h2', sectionE, {
+            text: 'MODULE ',
+            class: 'h2'
+        });
+        Util.createAndAppend('span', h2E, {
+            text: data.name,
+            id: 'span'
+        });
+        const divE = Util.createAndAppend('div', sectionE, {
+            id: 'repoInfo',
+        });
+        const moduleDetail = Util.createAndAppend('div', divE, {
+            class: 'moduleDetail'
+
+        });
+        Util.createAndAppend('a', moduleDetail, {
+            class: data.name,
+            href: data.html_url,
+            text: 'Github link'
+        });
+        Util.createAndAppend('p', moduleDetail, {
+            text: `Description: ${data.description}`
+        });
+        Util.createAndAppend('p', moduleDetail, {
+            text: `Forks : ${data.forks_count}`
+        });
+        Util.createAndAppend('p', moduleDetail, {
+            text: `Updated : ${data.updated_at}`
+        });
+        Util.createAndAppend('div', divE, {
+            class: 'contri',
+        });
+
     }
 
-    /**
-     * Returns an array of contributors as a promise
-     */
+
     fetchContributors() {
         return Util.fetchJSON(this.repository.contributors_url);
     }
