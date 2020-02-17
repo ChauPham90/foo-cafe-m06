8e03d3046cdc043bb675f8363c52d5e74f663ce2
ChauPham90 <minhchaupham90@gmail.com>
2020-02-17 21:50:26 +0100

    fixed server

--- React/font-end-todos/node.js
+++ React/font-end-todos/node.js
@@ -3,7 +3,9 @@ const server = require('fastify')({
     logger: true
 })
 var conString = "postgres://edgynoqk:ojK2CcvZglU39aSymmBIMP9m7SlD6GDs@otto.db.elephantsql.com:5432/edgynoqk"
-var client = new pg.Client(conString);
+var client = new pg.Client({
+    connectionString: conString
+});
 
 
 server.get("/", async (request, reply) => {
@@ -14,4 +16,17 @@ server.get("/", async (request, reply) => {
 
 const port = process.env.PORT || 5000
 
-server.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const boot = async () => {
+    await client.connect();
+    await server.listen(port);
+    //server.listen(port)
+};
+
+const onBootComplete = () => console.info("this app can be used now");
+const onBootUnComplete = err => console.error(`Step back,Bug!!!${err.message}`);
+boot()
+    .then(onBootComplete)
+    .catch(onBootUnComplete);
+// const port = process.env.PORT || 5000
+
+//server.listen(port, () => console.log(`Example app listening on port ${port}!`))
